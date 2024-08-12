1c69699a74fb69f925496630f05f6a5bcead1ada
Trilok Chand Swami <Trilok Chand Swami@ACER>
2024-08-12 09:45:21 +0530

    Done code for the ChatMessanger view.

---
+++ Controllers/MessangerController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Social_Media_Project.Controllers
+{
+    public class MessangerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ChatMessanger()
+        {
+            string Message = "";
+            try
+            {
+                return View();
+            }
+            catch (Exception ex)
+            {
+                Message = ex.Message;
+                return BadRequest(Message);
+            }
+        }
+    }
+}
